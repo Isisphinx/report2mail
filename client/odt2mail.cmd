cf208719f65a8992a41d2c117b05fec3b7de0a6b
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2020-02-11 20:37:12 +0100

    use docname without file extension

--- client/odt2mail.cmd
+++ client/odt2mail.cmd
@@ -4,11 +4,8 @@ set fname=%2
 set studyDate=%3
 set email=%4
 set reportPath=%5
+set docName=%~n5
 set InstitutionName=MyFacility
-for %%F in (%reportPath%) do set fileName=%%~nxF
 <Path to >soffice.exe --headless --convert-to pdf %reportPath%
 set SERVERADDR=reports.isis.care:443
-report2mail.exe "{\"emailAddress\":\"%email%\",\"firstname\":\"%fname%\",\"lastname\":\"%lname%\",\"date\":\"%studyDate%\",\"office\":\"%InstitutionName%\",\"fileLocation\":\"report.pdf\"}" >> log.txt
-
-REM %~n1 Expand %1 to a file Name without file extension or path
-REM %fileName:~0,-4%
+report2mail.exe "{\"emailAddress\":\"%email%\",\"firstname\":\"%fname%\",\"lastname\":\"%lname%\",\"date\":\"%studyDate%\",\"office\":\"%InstitutionName%\",\"fileLocation\":\"%docName%.pdf\"}" >> log.txt
