79d5e2261cb759b4e73d5ce3c54f222157551ac3
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2020-02-11 20:42:16 +0100

    redirect stderr to log

--- client/odt2mail.cmd
+++ client/odt2mail.cmd
@@ -8,4 +8,4 @@ set docName=%~n5
 set InstitutionName=MyFacility
 <Path to >soffice.exe --headless --convert-to pdf %reportPath%
 set SERVERADDR=reports.isis.care:443
-report2mail.exe "{\"emailAddress\":\"%email%\",\"firstname\":\"%fname%\",\"lastname\":\"%lname%\",\"date\":\"%studyDate%\",\"office\":\"%InstitutionName%\",\"fileLocation\":\"%docName%.pdf\"}" >> log.txt
+report2mail.exe "{\"emailAddress\":\"%email%\",\"firstname\":\"%fname%\",\"lastname\":\"%lname%\",\"date\":\"%studyDate%\",\"office\":\"%InstitutionName%\",\"fileLocation\":\"%docName%.pdf\"}" 2>> log.txt
